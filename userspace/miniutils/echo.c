5f98204424e338adef94410fe38c3f01d1ac4971
Samuel <sasdallas765@gmail.com>
2025-04-21 09:43:09 -0500

    userspace: (all) Ethereal Operating System rename

--- userspace/miniutils/echo.c
+++ userspace/miniutils/echo.c
@@ -4,7 +4,7 @@
  * 
  * 
  * @copyright
- * This file is part of the Hexahedron kernel, which is part of reduceOS.
+ * This file is part of the Hexahedron kernel, which is part of the Ethereal operating system.
  * It is released under the terms of the BSD 3-clause license.
  * Please see the LICENSE file in the main repository for more details.
  * 
