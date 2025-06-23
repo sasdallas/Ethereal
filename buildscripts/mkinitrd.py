ed90bb83f4bc8ebde61bcd7bbfe87dd7a080ef36
Samuel Stuart <sasdallas765@gmail.com>
2025-06-23 15:23:19 -0500

    build: (mkinitrd) Filter and set UID and GID to 0

--- buildscripts/mkinitrd.py
+++ buildscripts/mkinitrd.py
@@ -10,5 +10,9 @@ if len(sys.argv) < 3:
 file = sys.argv[1]
 dir = sys.argv[2]
 
+def ramdisk_filter(tarinfo):
+    tarinfo.uid = 0
+    tarinfo.gid = 0
+
 with tarfile.open(file, "w", format=tarfile.USTAR_FORMAT) as ramdisk:
-    ramdisk.add(dir, arcname="/")
+    ramdisk.add(dir, arcname="/", filter=ramdisk_filter)
