ace574d2db542aef90ba08128451d302e77d7a74
Samuel Stuart <sasdallas765@gmail.com>
2025-06-27 14:43:35 -0500

    libpolyhedron: (string) Rewrite strstr/memchr

--- libpolyhedron/string/memchr.c
+++ libpolyhedron/string/memchr.c
@@ -1,6 +1,6 @@
 /**
- * @file libpolyhedron/string/strstr.c
- * @brief strstr
+ * @file libpolyhedron/string/memchr.c
+ * @brief memchr
  * 
  * 
  * @copyright
@@ -13,15 +13,15 @@
 
 #include <string.h>
 
-char* strstr(const char* haystack, const char* needle) {
-    size_t l = strlen(needle);
-    if (l == 0) return (char*)haystack;
+void *memchr(const void *ptr, int ch, size_t count) {
+    unsigned char *p = (unsigned char*)ptr;
+    size_t idx = 0;
 
-    for (size_t i = 0; haystack[i]; i++) {
-		if (strncmp(haystack + i, needle, l) == 0) {
-            return ((char*)haystack + i);
-		}
-	}
-        
+    while (idx < count) {
+        if (*p == ch) return p;
+        p++;
+        idx++;
+    }
+
     return NULL;
 }
