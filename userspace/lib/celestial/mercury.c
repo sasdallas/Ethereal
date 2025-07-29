c4278eb68ab5555df992c15300b1f68b356e7937
Samuel Stuart <sasdallas765@gmail.com>
2025-07-29 13:30:22 -0500

    userspace: (lib:celestial) Rounded crners

--- userspace/lib/celestial/mercury.c
+++ userspace/lib/celestial/mercury.c
@@ -35,6 +35,10 @@ sprite_t *minimize_sprite = NULL;
 #define MERCURY_COLOR_TEXT_FOCUSED      (GFX_RGB(255, 255, 255))
 #define MERCURY_COLOR_TEXT_UNFOCUSED    (GFX_RGB(0xcc, 0xcc, 0xcc))
 
+/* Border colors */
+#define MERCURY_BORDER_GRADIENT_START           GFX_RGB(0x3f, 0x3b, 0x42)
+#define MERCURY_BORDER_GRADIENT_END             GFX_RGB(0x95, 0x90, 0x99)
+
 /**
  * @brief Initialize the Mercury theme
  * @param win The window to initialize Mercury on
@@ -50,39 +54,6 @@ int celestial_initMercury(window_t *win) {
 }
 
 /**
- * @brief Render the top/bottom gradient of Mercury
- * @param win The window to render the top gradient on
- * @param bottom Toggle to draw the gradient on the bottom
- */
-static void celestial_renderMercuryGradient(window_t *win, int bottom) {
-    // https://stackoverflow.com/questions/72756900/color-gradient-in-c (LMAO)
-    // Calculate the steps
-
-    gfx_color_t grad_start = GFX_RGB(0x3f, 0x3b, 0x42);
-    gfx_color_t grad_end = GFX_RGB(0x95, 0x90, 0x99);
-
-    float step_a = (float)(GFX_RGB_A(grad_end) - GFX_RGB_A(grad_start)) / (float)WIN_WIDTH(win);
-    float step_r = (float)(GFX_RGB_R(grad_end) - GFX_RGB_R(grad_start)) / (float)WIN_WIDTH(win);
-    float step_g = (float)(GFX_RGB_G(grad_end) - GFX_RGB_G(grad_start)) / (float)WIN_WIDTH(win);
-    float step_b = (float)(GFX_RGB_B(grad_end) - GFX_RGB_B(grad_start)) / (float)WIN_WIDTH(win);
-
-    for (uintptr_t x = 0; x < WIN_WIDTH(win); x++) {
-        gfx_color_t color = 0 |
-            ((uint8_t)(GFX_RGB_A(grad_start) + x * step_a) & 0xFF) << 24 |
-            ((uint8_t)(GFX_RGB_R(grad_start) + x * step_r) & 0xFF) << 16 |
-            ((uint8_t)(GFX_RGB_G(grad_start) + x * step_g) & 0xFF) << 8 |
-            ((uint8_t)(GFX_RGB_B(grad_start) + x * step_b) & 0xFF) << 0;
-
-        uintptr_t y = (bottom ? WIN_HEIGHT(win) - win->decor->borders.bottom_height : 0);
-        uintptr_t y_max = (bottom ? WIN_HEIGHT(win) : (size_t)win->decor->borders.top_height);
-
-        for (; y < y_max; y++) {
-            GFX_PIXEL(win->decor->ctx, x, y) = color;
-        }
-    }
-}
-
-/**
  * @brief Render the Mercury theme
  * @param win The window to render the Mercury theme on
  */
@@ -92,21 +63,22 @@ int celestial_renderMercury(window_t *win) {
     // Render our borders
     
     // General window border
-    gfx_rect_t rect_window_border = { .x = 0, .y = 0, .width = WIN_WIDTH(win) - 1, .height = WIN_HEIGHT(win) - 1};
     gfx_rect_t rect_inner_window_border = { .x = d->borders.left_width - 1, .y = d->borders.top_height - 1, .width = WIN_WIDTH(win)  - d->borders.left_width - d->borders.right_width + 1, .height = WIN_HEIGHT(win) - d->borders.top_height - d->borders.bottom_height + 1 };
 
     // Left border
-    gfx_rect_t rect_left = { .x = 0, .y = d->borders.top_height, .width = d->borders.left_width, .height = WIN_HEIGHT(win) - d->borders.top_height - d->borders.bottom_height };
-
+    gfx_rect_t rect_left = { .x = 0, .y = d->borders.top_height - 4, .width = d->borders.left_width, .height = WIN_HEIGHT(win) - d->borders.top_height - d->borders.bottom_height + 8 };
+    
     // Right border
-    gfx_rect_t rect_right = { .x = WIN_WIDTH(win) - d->borders.right_width, .y = d->borders.top_height, .width = d->borders.right_width - 1, .height = WIN_HEIGHT(win) - d->borders.top_height - d->borders.bottom_height, };
+    gfx_rect_t rect_right = { .x = WIN_WIDTH(win) - d->borders.right_width + 1, .y = d->borders.top_height - 4, .width = d->borders.right_width - 1, .height = WIN_HEIGHT(win) - d->borders.top_height - d->borders.bottom_height + 8, };
 
+    gfx_drawRectangleFilled(win->decor->ctx, &GFX_RECT(0, 0, win->info->width, d->borders.top_height), GFX_RGBA(0,0,0,0));
+    gfx_drawRoundedRectangleGradient(win->decor->ctx, &GFX_RECT(0, 0, win->info->width, d->borders.top_height), 4, GFX_GRADIENT_HORIZONTAL, MERCURY_BORDER_GRADIENT_START, MERCURY_BORDER_GRADIENT_END);
+    
+    gfx_drawRectangleFilled(win->decor->ctx, &GFX_RECT(0, win->info->height - d->borders.bottom_height, win->info->width, d->borders.bottom_height), GFX_RGBA(0,0,0,0));
+    gfx_drawRoundedRectangleGradient(win->decor->ctx, &GFX_RECT(0, win->info->height - d->borders.bottom_height, win->info->width, d->borders.bottom_height), 4, GFX_GRADIENT_HORIZONTAL, MERCURY_BORDER_GRADIENT_START, MERCURY_BORDER_GRADIENT_END);
 
-    celestial_renderMercuryGradient(win, 0);
-    celestial_renderMercuryGradient(win, 1);
     gfx_drawRectangleFilled(win->decor->ctx, &rect_left, MERCURY_COLOR_LEFT_BORDER);
     gfx_drawRectangleFilled(win->decor->ctx, &rect_right, MERCURY_COLOR_RIGHT_BORDER);
-    gfx_drawRectangle(win->decor->ctx, &rect_window_border, GFX_RGB(16,16,16));
     gfx_drawRectangle(win->decor->ctx, &rect_inner_window_border, GFX_RGB(16,16,16));
     gfx_renderSprite(win->decor->ctx, close_sprite, win->decor->ctx->width - close_sprite->width - 4, 2);
     gfx_renderSprite(win->decor->ctx, maximize_sprite, win->decor->ctx->width - close_sprite->width - maximize_sprite->width - 5, 2);
