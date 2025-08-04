c7f965a64fd626d15613e4499e7d7fccfec6c898
Samuel Stuart <sasdallas765@gmail.com>
2025-08-04 11:12:36 -0500

    hexahedron: (misc:mutex) Initial commit

---
+++ hexahedron/include/kernel/misc/mutex.h
@@ -0,0 +1,59 @@
+/**
+ * @file hexahedron/include/kernel/misc/mutex.h
+ * @brief Mutex system for kernel
+ * 
+ * 
+ * @copyright
+ * This file is part of the Hexahedron kernel, which is part of the Ethereal Operating System.
+ * It is released under the terms of the BSD 3-clause license.
+ * Please see the LICENSE file in the main repository for more details.
+ * 
+ * Copyright (C) 2025 Samuel Stuart
+ */
+
+#ifndef KERNEL_MISC_MUTEX_H
+#define KERNEL_MISC_MUTEX_H
+
+/**** INCLUDES ****/
+#include <stdint.h>
+#include <stdatomic.h>
+#include <kernel/task/sleep.h>
+#include <sys/types.h>
+
+/**** TYPES ****/
+
+typedef struct mutex {
+    char *name;             // Optional mutex name
+    volatile pid_t lock;    // Mutex lock
+    sleep_queue_t *queue;   // Sleep queue
+} mutex_t;
+
+/**** FUNCTIONS ****/
+
+/**
+ * @brief Create a new mutex
+ * @param name Optional mutex name
+ */
+mutex_t *mutex_create(char *name);
+
+/**
+ * @brief Acquire the mutex
+ * @param mutex The mutex to acquire
+ */
+void mutex_acquire(mutex_t *mutex);
+
+/**
+ * @brief Try to acquire the mutex
+ * @param mutex The mutex to try to acquire
+ * @returns 1 on successful acquire
+ */
+int mutex_tryAcquire(mutex_t *mutex);
+
+/**
+ * @brief Release the mutex
+ * @param mutex The mutex to release
+ */
+void mutex_release(mutex_t *mutex);
+
+
+#endif
---
+++ hexahedron/misc/mutex.c
@@ -0,0 +1,69 @@
+/**
+ * @file hexahedron/misc/mutex.c
+ * @brief Kernel mutex implementation
+ * 
+ * 
+ * @copyright
+ * This file is part of the Hexahedron kernel, which is part of the Ethereal Operating System.
+ * It is released under the terms of the BSD 3-clause license.
+ * Please see the LICENSE file in the main repository for more details.
+ * 
+ * Copyright (C) 2025 Samuel Stuart
+ */
+
+#include <kernel/misc/mutex.h>
+#include <kernel/mem/alloc.h>
+#include <kernel/task/process.h>
+#include <string.h>
+
+/**
+ * @brief Create a new mutex
+ * @param name Optional mutex name
+ */
+mutex_t *mutex_create(char *name) {
+    mutex_t *m = kzalloc(sizeof(mutex_t));
+    __atomic_store_n(&(m->lock), -1, __ATOMIC_SEQ_CST);
+
+    m->name = name;
+    m->queue = sleep_createQueue("queue");
+    return m;
+}
+
+/**
+ * @brief Acquire the mutex
+ * @param mutex The mutex to acquire
+ */
+void mutex_acquire(mutex_t *mutex) {
+    pid_t expect = (pid_t)-1;
+    pid_t want = (current_cpu->current_thread ? current_cpu->current_thread->tid : 0); // 0 is the kernel reserved TID 
+    while (!__atomic_compare_exchange_n(&mutex->lock, &expect, want, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
+        if (current_cpu->current_thread) {
+            sleep_inQueue(mutex->queue);
+            sleep_enter();
+        } else {
+            arch_pause_single();
+        }
+
+        expect = (pid_t)-1;
+    }
+}
+
+/**
+ * @brief Try to acquire the mutex
+ * @param mutex The mutex to try to acquire
+ * @returns 1 on successful acquire
+ */
+int mutex_tryAcquire(mutex_t *mutex) {
+    pid_t expect = (pid_t)-1;
+    pid_t want = (current_cpu->current_thread ? current_cpu->current_thread->tid : 0); // 0 is the kernel reserved TID 
+    return __atomic_compare_exchange_n(&mutex->lock, &expect, want, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+}
+
+/**
+ * @brief Release the mutex
+ * @param mutex The mutex to release
+ */
+void mutex_release(mutex_t *mutex) {
+    __atomic_store_n(&mutex->lock, -1, __ATOMIC_SEQ_CST);
+    sleep_wakeupQueue(mutex->queue, 1);
+}
