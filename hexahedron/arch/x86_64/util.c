9cac270e1c43fd6d459f8bc6897f40ab30cfdfbd
Samuel Stuart <sasdallas765@gmail.com>
2025-08-14 09:23:07 -0500

    hexahedron: (x86_64:util) Remove /kernel/pat (ToaruOS)

--- hexahedron/arch/x86_64/util.c
+++ hexahedron/arch/x86_64/util.c
@@ -89,55 +89,6 @@ void arch_initialize_context(struct thread *thread, uintptr_t entry, uintptr_t s
 }
 
 /**
- * @brief /kernel/pat entry in the kernelfs
- */
-static int arch_pat_kernelfs(struct kernelfs_entry *entry, void *data) {
-	// https://github.com/klange/toaruos/blob/1a551db5cf47aa8ae895983b2819d5ca19be53a3/kernel/vfs/procfs.c#L367
-    uint32_t pat_value_low, pat_value_high;
-	asm volatile ( "rdmsr" : "=a" (pat_value_low), "=d" (pat_value_high): "c" (0x277) );
-	uint64_t pat_values = ((uint64_t)pat_value_high << 32) | (pat_value_low);
-
-	const char * pat_names[] = {
-		"uncacheable (UC)",
-		"write combining (WC)",
-		"Reserved",
-		"Reserved",
-		"write through (WT)",
-		"write protected (WP)",
-		"write back (WB)",
-		"uncached (UC-)"
-	};
-
-	int pa_0 = (pat_values >>  0) & 0x7;
-	int pa_1 = (pat_values >>  8) & 0x7;
-	int pa_2 = (pat_values >> 16) & 0x7;
-	int pa_3 = (pat_values >> 24) & 0x7;
-	int pa_4 = (pat_values >> 32) & 0x7;
-	int pa_5 = (pat_values >> 40) & 0x7;
-	int pa_6 = (pat_values >> 48) & 0x7;
-	int pa_7 = (pat_values >> 56) & 0x7;
-
-	kernelfs_writeData(entry,
-			"PA0: %d %s\n"
-			"PA1: %d %s\n"
-			"PA2: %d %s\n"
-			"PA3: %d %s\n"
-			"PA4: %d %s\n"
-			"PA5: %d %s\n"
-			"PA6: %d %s\n"
-			"PA7: %d %s\n",
-			pa_0, pat_names[pa_0],
-			pa_1, pat_names[pa_1],
-			pa_2, pat_names[pa_2],
-			pa_3, pat_names[pa_3],
-			pa_4, pat_names[pa_4],
-			pa_5, pat_names[pa_5],
-			pa_6, pat_names[pa_6],
-			pa_7, pat_names[pa_7]);
-    return 0;
-}
-
-/**
  * @brief /kernel/cpus/XXX in the kernelfs
  */
 static int arch_cpu_kernelfs(struct kernelfs_entry *entry, void *data) {
@@ -168,8 +119,6 @@ static int arch_cpu_kernelfs(struct kernelfs_entry *entry, void *data) {
  * @brief Mount KernelFS nodes
  */
 void arch_mount_kernelfs() {
-    kernelfs_createEntry(NULL, "pat", arch_pat_kernelfs, NULL);
-	
 	kernelfs_dir_t *dir = kernelfs_createDirectory(NULL, "cpus", 1);
 	for (int i = 0; i < MAX_CPUS; i++) {
 		if (processor_data[i].cpu_id || !i) {
