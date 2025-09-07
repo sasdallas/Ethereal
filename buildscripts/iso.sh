2369883e5c2dca9ca70a85d5f0ba3beac69ef26b
Samuel Stuart <sasdallas765@gmail.com>
2025-09-07 09:53:14 -0500

    conf: (theme) GRUB theme initial commit (based off of Starfield and Particle Circle)

--- buildscripts/iso.sh
+++ buildscripts/iso.sh
@@ -15,13 +15,18 @@ cd $PROJECT_ROOT
 
 # Create ISO directories
 mkdir -pv $ISO_OUTPUT_DIRECTORY/iso/boot/grub/
+mkdir -pv $ISO_OUTPUT_DIRECTORY/iso/boot/grub/themes
+mkdir -pv $ISO_OUTPUT_DIRECTORY/iso/boot/grub/fonts/
 cp $SYSROOT/$BOOT_DIRECTORY/hexahedron-kernel.elf $ISO_OUTPUT_DIRECTORY/iso/boot/
 cp $SYSROOT/$BOOT_DIRECTORY/initrd.tar.img $ISO_OUTPUT_DIRECTORY/iso/boot/
+cp -r conf/theme $ISO_OUTPUT_DIRECTORY/iso/boot/grub/themes/ethereal/
 cp conf/grub.cfg $ISO_OUTPUT_DIRECTORY/iso/boot/grub/
 cp -r conf/extra-boot-files/* $ISO_OUTPUT_DIRECTORY/iso/boot/ || true
 
+mv $ISO_OUTPUT_DIRECTORY/iso/boot/grub/themes/ethereal/*.pf2 $ISO_OUTPUT_DIRECTORY/iso/boot/grub/fonts/
+
 # Pack it into an ISO
-grub-mkrescue -o $ISO_OUTPUT_DIRECTORY/hexahedron.iso $ISO_OUTPUT_DIRECTORY/iso
+grub-mkrescue -o $ISO_OUTPUT_DIRECTORY/hexahedron.iso $ISO_OUTPUT_DIRECTORY/iso --themes=starfield
 
 # Remove the old directory
 rm -r $ISO_OUTPUT_DIRECTORY/iso/
