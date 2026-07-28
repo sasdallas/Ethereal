5cb7700fc12340a1d906132c77b6cd033cb8b368
Samuel Stuart <sasdallas765@gmail.com>
2026-07-28 09:51:00 -0500

    hexahedron: (klib:string) Fix for updated mlibc

--- hexahedron/klib/string/select.c
+++ hexahedron/klib/string/select.c
@@ -8,16 +8,16 @@
 
 void __FD_CLR(int fd, fd_set *set) {
 	assert(fd < FD_SETSIZE);
-	set->fds_bits[fd / 8] &= ~(1 << (fd % 8));
+	set->__fds_bits[fd / 8] &= ~(1 << (fd % 8));
 }
 int __FD_ISSET(int fd, fd_set *set) {
 	assert(fd < FD_SETSIZE);
-	return set->fds_bits[fd / 8] & (1 << (fd % 8));
+	return set->__fds_bits[fd / 8] & (1 << (fd % 8));
 }
 void __FD_SET(int fd, fd_set *set) {
 	assert(fd < FD_SETSIZE);
-	set->fds_bits[fd / 8] |= 1 << (fd % 8);
+	set->__fds_bits[fd / 8] |= 1 << (fd % 8);
 }
 void __FD_ZERO(fd_set *set) {
-	memset(set->fds_bits, 0, sizeof(fd_set));
+	memset(set->__fds_bits, 0, sizeof(fd_set));
 }
